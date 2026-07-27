b5ca1d50066f46efed3c70049715864368b19442
Jannik034 <96433458+Jannik034@users.noreply.github.com>
2026-07-27 20:34:39 +0200

    Add primeGenerator (sieve-function) and mapPrimesOnSlider logic

--- src/main/java/com/example/rsaverfahren/Controller.java
+++ src/main/java/com/example/rsaverfahren/Controller.java
@@ -1,20 +1,59 @@
 package com.example.rsaverfahren;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
+
 
 public class Controller {
-    @FXML
-    private TabPane tabPane;
 
-    @FXML
-    protected void initialize() {
-        tabPane.getTabs().setAll(
-                new Tab("Verschlüsseln", new Label("Verschlüsseln")),
-                new Tab("Entschlüsseln", new Label("Entschlüsseln")),
-                new Tab("Brute Force Attacke", new Label("Brute Force Attacke"))
-        );
+    @FXML private Slider sliderP, sliderQ;
+    @FXML private Label labelP, labelQ;
+
+    @FXML private void initialize() {
+        int maxPrimePossible = 1000;
+        int[] primes = primeGenerator(maxPrimePossible);
+        mapPrimesOnSlider(primes, sliderP, labelP);
+        mapPrimesOnSlider(primes, sliderQ, labelQ);
+        sliderP.valueProperty().addListener((obs, o, n) -> mapPrimesOnSlider(primes, sliderP, labelP));
+        sliderQ.valueProperty().addListener((obs, o, n) -> mapPrimesOnSlider(primes, sliderQ, labelQ));
+    }
+
+    @FXML private void mapPrimesOnSlider(int[] primes, Slider slider, Label label) {
+
+        int p = primes[(int) Math.round(slider.getValue())];
+
+        slider.setMin(0);
+        slider.setMax(primes.length -1);
+        slider.setSnapToTicks(true);
+        slider.setMinorTickCount(0);
+        slider.setBlockIncrement(1);
+        slider.setShowTickLabels(false);
+        slider.setShowTickMarks(false);
+
+        label.setText(""+p);
+    }
+
+    static int[] primeGenerator(int max) {
+        boolean[] composite = new boolean[max + 1];
+        int count = 0;
+
+        for(int i = 2; i*i <= max; i++) {
+            if(!composite[i]) {
+                for(int j = i*i; j <= max; j += i) {
+                    composite[j] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i <= max; i++) {
+            if (!composite[i]) { count++;}
+        }
+
+        int[] primes = new int[count];
+        for (int i = 2, k = 0; i <= max; i++) {
+            if (!composite[i]) primes[k++] = i;
+        }
+
+        return primes;
     }
 }
